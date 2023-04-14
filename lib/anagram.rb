f08559fb79159628e91b82e8f8672c348140b113
Patrick Mason <patrickmason73@gmail.com>
2023-04-13 21:54:10 -0600

    commit tree 90ed5cd5719dbb52118ee334460f6856b26337be

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram 
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+       array.find_all do |item| 
+        item.chars.sort == @word.chars.sort
+    end
+    end
+end 
